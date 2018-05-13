32e25794d5689091cf3187416f1e2280261603ee
Heitor Henrique Vieira Guimaraes <heitorhvg@outlook.com>
2018-05-12 23:20:49 -0300

    conflito resolvido

--- index.php
+++ index.php
@@ -1,13 +1,7 @@
 <?php
 	
-<<<<<<< HEAD
 	echo "Olá Mundo456!";
   
-  echo "Olá Udemy!!"
-=======
-	echo "Olá Mundo 123!";
-
-	echo "Olá Hcode!";
->>>>>>> 673533ef8189d4bc33332901008f58e9174e8e6a
+  echo "Olá Udemy e Olá Hcode!!"
 
 ?>
